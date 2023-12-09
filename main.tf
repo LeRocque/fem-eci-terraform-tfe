88ba305584862befe4aebb003f178a705b13d207
LeRocque <t.lerocque@gmail.com>
2023-12-09 15:54:12 -0500

    added locals

--- main.tf
+++ main.tf
@@ -1,17 +1,19 @@
 module "project" {
+  for_each = local.project
   source  = "ALT-F4-LLC/project/tfe"
   version = "0.5.0"
-  description = "Example"
-  name = "fem-eci-project"
+  description = each.value.description
+  name = each.key
   organization_name = var.organization_name
 }
 
 module "workspace" {
+  for_each = local.workspace
   source  = "ALT-F4-LLC/workspace/tfe"
   version = "0.8.0"
-  description = "Example"
+  description = each.value.description
   execution_mode = "local"
-  name = "fem-eci-workspace"
+  name = each.key
   organization_name = var.organization_name
-  project_id = module.project.id
+  project_id = each.value.project_id
 }
