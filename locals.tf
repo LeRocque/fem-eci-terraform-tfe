856a49611b63575913e2e6dbb675aca896e01715
LeRocque <t.lerocque@gmail.com>
2023-12-10 14:11:49 -0500

    setup github workspace

--- locals.tf
+++ locals.tf
@@ -6,10 +6,16 @@ locals {
   }
   workspace = {
     "fem-eci-tfe" = {
-      description = "Example"
+      description = "Example description of workspace"
       execution_mode = "remote"
       project_id = module.project["fem-eci-project"].id
-      vcs_repo_identifier = "LeRocque/fem-eci-terraform-tfe"
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
+    }
+    "fem-eci-github" = {
+      description = "Example automation workspace for Github resources"
+      execution_mode = "local"
+      project_id = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
     }
   }
 }
