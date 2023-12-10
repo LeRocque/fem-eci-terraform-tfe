55b2c65b69090e1b2408ae4902d34418a8c9fc17
LeRocque <t.lerocque@gmail.com>
2023-12-10 13:30:00 -0500

    added data.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "organization_name" {
   default = "LeRocque"
   type    = string
 }
+
+variable "github_app_installation_id" {
+  default = 44940721
+  type = number
+}
+
+variable "github_organization_name" {
+  default = "LeRocque"
+  type = string
+}
